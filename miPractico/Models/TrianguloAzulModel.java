8e7a3b0affb211e90d7e356dcdb6485ed31e6849
SandraS2611 <sssandra1980@gmail.com>
2024-10-02 02:04:42 -0300

    pinta mejor falta que no desordene la matriz

--- miPractico/Models/TrianguloAzulModel.java
+++ miPractico/Models/TrianguloAzulModel.java
@@ -5,13 +5,25 @@ public class TrianguloAzulModel {
     public void pintarTrianguloAzul(int[][] matriz) {
         int filas = matriz.length;
 
+        // la base de mi triangulo
+        int base = (filas * 2) - 1;
+
         for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < filas - i - 1; j++) {
-                System.out.print(" ");
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = 0;
             }
-            for (int k = 0; k <= i; k++) {
+        }
+
+        // pinto la base
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < (2 * i + 1); j++) {
+                int centro = (base / 2) - i + j;
+
+                if (centro >= 0 && centro < matriz[i].length) {
+                    matriz[i][centro] = 1;
+                }
             }
-            System.out.println();
         }
     }
+
 }
