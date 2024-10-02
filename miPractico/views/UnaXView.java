2ff0795f555d3d72ca52765a82645abaf5e145ea
SandraS2611 <sssandra1980@gmail.com>
2024-10-02 01:26:55 -0300

    UnaXView solo le agregue color de fondo a las casillas

--- miPractico/views/UnaXView.java
+++ miPractico/views/UnaXView.java
@@ -1,8 +1,10 @@
 package miPractico.Views; 
 
 public class UnaXView {
-    public static final String ANSI_PURPLE = "\u001B[35m";  // Color para la diagonal principal
-    public static final String ANSI_YELLOW = "\u001B[33m";    // Color para la diagonal secundaria
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+ // Color para la diagonal principal
+ public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+ // Color para la diagonal secundaria
     public static final String ANSI_RESET = "\u001B[0m";     // Reset de color
 
     public void pintarValoresEnMatriz(int[][] matriz) {
@@ -13,11 +15,11 @@ public class UnaXView {
             for (int j = 0; j < matriz[i].length; j++) {
                 // busca la 1er diagonal 
                 if (j == i) {  
-                    System.out.print(ANSI_PURPLE + matriz[i][j] + ANSI_RESET + " ");
+                    System.out.print(ANSI_BLUE_BACKGROUND + matriz[i][j] + ANSI_RESET + " ");
                 } 
                 // busca la 2da diagonal
                 else if (j == (matriz.length - 1 - i)) {  
-                    System.out.print(ANSI_YELLOW + matriz[i][j] + ANSI_RESET + " ");
+                    System.out.print(ANSI_YELLOW_BACKGROUND + matriz[i][j] + ANSI_RESET + " ");
                 } 
                 // Si no imprime el número normalmente
                 else {
