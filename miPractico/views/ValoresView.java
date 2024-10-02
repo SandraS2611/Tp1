20358f5a0d7d1d65979ae6dbf6cf9ae22ea000b6
SandraS2611 <sssandra1980@gmail.com>
2024-10-02 01:29:23 -0300

    fondo de color a los casilleros

--- miPractico/views/ValoresView.java
+++ miPractico/views/ValoresView.java
@@ -1,9 +1,8 @@
 package miPractico.Views;
 
 public class ValoresView {
-
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
     public static final String ANSI_RESET = "\u001B[0m";
 
     public void pintarValoresEnMatriz(int[][] matriz, int mayorValor, int menorValor) {
@@ -13,9 +12,9 @@ public class ValoresView {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 if (matriz[i][j] == mayorValor) {
-                    System.out.print(ANSI_RED + matriz[i][j] + ANSI_RESET + " ");
+                    System.out.print(ANSI_CYAN_BACKGROUND + matriz[i][j] + ANSI_RESET + " ");
                 } else if (matriz[i][j] == menorValor) {
-                    System.out.print(ANSI_CYAN + matriz[i][j] + ANSI_RESET + " ");
+                    System.out.print(ANSI_RED_BACKGROUND + matriz[i][j] + ANSI_RESET + " ");
                 } else {
                     System.out.print(matriz[i][j] + " ");
                 }
