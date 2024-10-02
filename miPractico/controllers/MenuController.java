1ea2b44b04350cbb0c1e4ece301d5789fd04e335
SandraS2611 <sssandra1980@gmail.com>
2024-10-02 02:19:57 -0300

    elimine lo que no corresponde al desafio 4

--- miPractico/controllers/MenuController.java
+++ miPractico/controllers/MenuController.java
@@ -7,9 +7,6 @@ public class MenuController {
 
     private MenuView view;
     private MatrizController matrizController;
-    private ValoresController valoresController;
-    private UnaXController unaXController;
-    private FilasParesCotroller filasParesCotroller;
     private CaracolController caracolController;
 
 
@@ -18,9 +15,6 @@ public void execute() {
         //int [][] matriz = matrizController.getMatriz();
         view = new MenuView();
         matrizController = new MatrizController();
-        valoresController = new ValoresController();
-        unaXController = new UnaXController();
-        filasParesCotroller = new FilasParesCotroller();
         caracolController = new CaracolController();
         
         do{
@@ -28,42 +22,12 @@ public void execute() {
             opciones = view.elegirOpciones();
 
             switch (opciones) {
-                case 1:
-                    view.mensajeEnPantalla(" Desafío 1: Pintar los Valores Mínimo y Máximo");
-                    matrizController.pedirFilasyColumnas();
-                    matrizController.generarYMostrarMatriz();
-                    //uso el controlador para pintar el número mas chico y el más grande
-                    valoresController.calcularYMostrarValores(matrizController.getMatriz());
-                    break;
-
-                    case 2:
-                    view.mensajeEnPantalla(" Desafío 2: Dibujar una X");
-                    matrizController.pedirFilasyColumnas();
-                    matrizController.generarYMostrarMatriz();
-                    //uso el controlador para pintar la "X"
-                    unaXController.pintarX(matrizController.getMatriz());
-                    break;
-
-                    case 3:
-                    view.mensajeEnPantalla(" Desafío 3: Pintar las Filas Pares");
-                    matrizController.pedirFilasyColumnas();
-                    matrizController.generarYMostrarMatriz();
-                    //uso el controlador para pintar las filas pares
-                    filasParesCotroller.pintarFilasParesEnMatriz(matrizController.getMatriz());
-                    break;
-
                     case 4:
                     view.mensajeEnPantalla(" Desafío 4: Pintar un Caracol");
                     matrizController.pedirFilasyColumnas();
                     matrizController.generarYMostrarMatriz();                   
                     //uso el controlador para pintar el caracol
                     caracolController.pintarCaracolEnMatriz(matrizController.getMatriz());
-                    break;
-
-                    case 5:
-                    view.mensajeEnPantalla(" Desafío 5: Pintar un Triángulo Azul");
-                    matrizController.pedirFilasyColumnas();
-                    matrizController.generarYMostrarMatriz();
                     break;
 
                     case 6:
--- miPractico/views/MatrizView.java
+++ miPractico/views/MatrizView.java
@@ -3,6 +3,9 @@ package miPractico.Views;
 public class MatrizView {
     public void mostrarMatriz(int[][] datos) {
         System.out.println("");
+        System.out.println("Matriz: ");
+        System.out.println();
+        
         for (int i = 0; i < datos.length; i++) {
             for (int j = 0; j < datos[i].length; j++) {
                 System.out.print(datos[i][j] + " ");
