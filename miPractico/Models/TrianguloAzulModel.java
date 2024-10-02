0d07dc066a1774670d54c062e79731aefebfd60b
SandraS2611 <sssandra1980@gmail.com>
2024-10-02 02:10:46 -0300

    error persistente

--- miPractico/Models/TrianguloAzulModel.java
+++ miPractico/Models/TrianguloAzulModel.java
@@ -5,20 +5,11 @@ public class TrianguloAzulModel {
     public void pintarTrianguloAzul(int[][] matriz) {
         int filas = matriz.length;
 
-        // la base de mi triangulo
-        int base = (filas * 2) - 1;
-
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = 0;
-            }
-        }
-
-        // pinto la base
+        // pinto el triangulo
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < (2 * i + 1); j++) {
-                int centro = (base / 2) - i + j;
-
+                
+                int centro = (filas - 1) - i + j;
                 if (centro >= 0 && centro < matriz[i].length) {
                     matriz[i][centro] = 1;
                 }
